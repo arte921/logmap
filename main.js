f64eb2580251fb1d9de8e94d45c626a03498614b
Ard-Jan van Etten <arte@linux.fritz.box>
2020-01-31 16:03:57 +0100

    canvas in a var now

--- main.js
+++ main.js
@@ -1,11 +1,16 @@
-var mcbwidth = document.getElementById('canvas').width;
-var mcbheight = document.getElementById('canvas').height;
+var canvas = document.getElementById('canvas');
+var ctx = canvas.getContext('2d');
+
+var mcbwidth = canvas.width;
+var mcbheight = canvas.height;
 
 var cx = 3.2;
 var rx = 0.5;
 var cy = 0.75;
 var ry = 0.25;
 
+var xres = 2*rx/mcbwidth;
+var yres = 2*ry/mcbheight;
 
 function plot(x,y){
 
