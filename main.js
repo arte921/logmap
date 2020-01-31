2391b4d907fe6d69a27df77da3d49c76cbe8b3da
Ard-Jan van Etten <arte@linux.fritz.box>
2020-01-31 15:50:07 +0100

    default r and r variables, added the lib in html

--- main.js
+++ main.js
@@ -1,9 +1,14 @@
 var mcbwidth = 800;
 var mcbheight = 800;
 
+var cx = 3.2;
+var rx = 0.5;
+var cy = 0.75;
+var ry = 0.25;
+
 const gpu = new GPU();
 const render = gpu.createKernel(function(mcbwidth,mcbheight,cx,cy,rx,ry){
-	
+
 }).setOutput([mcbwidth,mcbheight]).setGraphical(true);
 
 render();
