dd1cae4cb0ac9a2819e5878f47c1f43b386539ff
Ard-Jan van Etten <arte@linux.fritz.box>
2020-01-31 16:37:18 +0100

    first working version

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-var pxx,pxy;
+var pxx,pxy,x,y,i;
 
 var canvas = document.getElementById('canvas');
 var ctx = canvas.getContext('2d');
@@ -8,15 +8,25 @@ var mcbheight = canvas.height;
 
 var cx = 3.2;
 var rx = 0.5;
-var cy = 0.75;
-var ry = 0.25;
+var cy = 0.5;
+var ry = 0.5;
 
 var xres = 2*rx/mcbwidth;
 var yres = 2*ry/mcbheight;
 
-function plot(x,y){
-	pxx = (x-(cx-rx))/(2*rx)*mcbwidth;
-	pxy = mcbheight-((y-(cy-ry))/(2*ry)*mcbheight);
-	console.log(pxx,pxy);
+function plot(mx,my){
+	pxx = (mx-(cx-rx))/(2*rx)*mcbwidth;
+	pxy = mcbheight-((my-(cy-ry))/(2*ry)*mcbheight);
 	ctx.fillRect(pxx,pxy,1,1);
+}
+
+x=cx-rx;
+y=0.5;
+while (x<=cx+rx){
+	for(i=0;i<100;i++){
+		y=x*y*(1-y);
+		plot(x,y);
+		console.log(x,y);
+	}
+	x += xres;
 }
