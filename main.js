e6469d4af5cfb58a9565a43ffb6a43ba9edf50c1
Ard-Jan van Etten <arte@linux.fritz.box>
2020-01-31 15:59:56 +0100

    added canvas, cleaned up a lil

--- main.js
+++ main.js
@@ -1,13 +1,12 @@
-var mcbwidth = 800;
-var mcbheight = 800;
+var mcbwidth = document.getElementById('canvas').width;
+var mcbheight = document.getElementById('canvas').height;
 
 var cx = 3.2;
 var rx = 0.5;
 var cy = 0.75;
 var ry = 0.25;
 
-render(mcbwidth,mcbheight,cx,cy,rx,ry){
+
+function plot(x,y){
 
 }
-
-render();
