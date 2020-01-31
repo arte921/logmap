836a72893bd4923525315f3a35473da266ce2348
Ard-Jan van Etten <arte@linux.fritz.box>
2020-01-31 15:52:41 +0100

    ermoved gpu

--- main.js
+++ main.js
@@ -6,10 +6,8 @@ var rx = 0.5;
 var cy = 0.75;
 var ry = 0.25;
 
-const gpu = new GPU();
-const render = gpu.createKernel(function(mcbwidth,mcbheight,cx,cy,rx,ry){
+render(mcbwidth,mcbheight,cx,cy,rx,ry){
 
-}).setOutput([mcbwidth,mcbheight]).setGraphical(true);
+}
 
 render();
-document.getElementById('mainframe').appendChild(render.canvas);
